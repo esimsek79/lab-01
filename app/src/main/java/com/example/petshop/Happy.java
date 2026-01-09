40dbd80c196f8744e0684859546f8abc34319ee5
ege simsek <egesimsek18@gmail.com>
2026-01-09 10:21:39 -0700

    first commit

---
+++ app/src/main/java/com/example/petshop/Happy.java
@@ -0,0 +1,13 @@
+package com.example.petshop;
+
+import java.util.Date;
+public class Happy extends Mood {
+    public Happy(Date date) {
+        super(date);
+    }
+
+    @Override
+    public String mood() {
+        return "Happy";
+    }
+}
